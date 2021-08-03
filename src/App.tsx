d61c17e6f8fc0c9296efdca23f6af548a6a85ed8
clemkeirua <clement@keiruaprod.fr>
2021-08-03 16:25:56 +0200

    extracted font-awesome to index

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React, { Component, MouseEvent } from 'react';
 import './App.css';
-import { library } from "@fortawesome/fontawesome-svg-core"
-import { fas } from "@fortawesome/free-solid-svg-icons"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-library.add(fas)
 
 type RngAppState = {
   n: number,
