491aaecd69de40a1599a312a14bd78dd0386b086
clemkeirua <clement@keiruaprod.fr>
2022-06-18 14:05:00 +0200

    generate moved

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -3,6 +3,29 @@ import './Game.css';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
+let id_mapping = [
+  {sound:"", color: "#0F0", text:"Pied gauche, vert"},
+  {sound:"", color: "yellow", text:"Pied gauche, jaune"},
+  {sound:"", color: "#00F", text:"Pied gauche, bleu"},
+  {sound:"", color: "#f00", text:"Pied gauche, rouge"},
+  {sound:"", color: "#FFF", text:"Pied gauche, en l’air"},
+  {sound:"", color: "#0F0", text:"Pied droit, vert"},
+  {sound:"", color: "yellow", text:"Pied droit, jaune"},
+  {sound:"", color: "#00F", text:"Pied droit, bleu"},
+  {sound:"", color: "#f00", text:"Pied droit, rouge"},
+  {sound:"", color: "#FFF", text:"Pied droit, en l’air"},
+  {sound:"", color: "#0F0", text:"Main gauche, vert"},
+  {sound:"", color: "yellow", text:"Main gauche, jaune"},
+  {sound:"", color: "#00F", text:"Main gauche, bleu"},
+  {sound:"", color: "#f00", text:"Main gauche, rouge"},
+  {sound:"", color: "#FFF", text:"Main gauche, en l’air"},
+  {sound:"", color: "#0F0", text:"Main droite, vert"},
+  {sound:"", color: "yellow", text:"Main droite, jaune"},
+  {sound:"", color: "#00F", text:"Main droite, bleu"},
+  {sound:"", color: "#f00", text:"Main droite, rouge"},
+  {sound:"", color: "#FFF", text:"Main droite, en l’air"},
+];
+
 type RngAppState = {
   n: number
 }
@@ -16,16 +39,15 @@ function getRandomInt(min:number, max:number): number {
 class Game extends Component<{}, RngAppState> {
   constructor(props: RngAppState) {
     super(props)
-    let n = getRandomInt(1, 16);
+    let n = getRandomInt(0, id_mapping.length-1);
     this.state = {
       n: n
     };
     this.repeatHandler = this.repeatHandler.bind(this)
     this.refreshHandler = this.refreshHandler.bind(this)
-    // this.refreshHandler()
   }
   refresh() {
-    let n = getRandomInt(1, 16)
+    let n = getRandomInt(0, id_mapping.length-1)
     this.setState({
       n: n
     });
@@ -33,11 +55,6 @@ class Game extends Component<{}, RngAppState> {
 
   repeatHandler(e: MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
-    // this.setState({
-    //   display_solution: true,
-    //   n: this.state.n,
-    //   solution: this.state.n*this.state.n,
-    // });
   }
 
   refreshHandler(e: MouseEvent<HTMLButtonElement>) {
@@ -49,7 +66,7 @@ class Game extends Component<{}, RngAppState> {
     let title, body;
     let c = "text-7xl sm:text-7xl lg:text-7xl leading-none font-extrabold tracking-tight text-gray-900 mt-10 mb-8 sm:mt-14 sm:mb-10";
     let c2 = "text-3xl sm:text-3xl lg:text-3xl leading-none font-extrabold tracking-tight text-gray-900 mt-10 mb-8 sm:mt-14 sm:mb-10";
-    title = <h1 className={c}>{this.state.n}</h1>
+    title = <h1 className={c}>{id_mapping[this.state.n].text}</h1>
     body = <div className="grid grid-cols-2">
       <button className={c2} onClick={this.repeatHandler}>
         <FontAwesomeIcon icon={["fas", "clipboard"]} /> Répéter
